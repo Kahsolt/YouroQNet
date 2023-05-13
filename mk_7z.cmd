ad63c104bae2959d64e19023a5cafb30a0ecd707
Kahsolt <kahsolt@qq.com>
2023-05-13 12:53:17 +0800

    add mk_7z

---
+++ mk_7z.cmd
@@ -0,0 +1,21 @@
+7z a -t7z ^
+  -mx9 ^
+  -xr!*.7z ^
+  -xr!mk_7z.cmd ^
+  -xr!ref\*.pptx ^
+  -xr!ref\*.*.pdf ^
+  -xr!repo\ ^
+  -xr!data\*.bin ^
+  -xr!data\*_cleaned.csv ^
+  -xr!data\valid.csv ^
+  -xr!data\unknown.csv ^
+  -xr!log\ ^
+  -xr!.git\ ^
+  -xr!.vscode\ ^
+  -xr!__pycache__\ ^
+  YouroQNet.7z ^
+  .
+
+7z u -t7z ^
+  -ir!repo\*.cmd ^
+  YouroQNet.7z
